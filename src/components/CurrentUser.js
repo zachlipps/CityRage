36d25adb7f33b6ab1a8dd0e541779b0742625d97
Zach Lipps <zacharylipps10@gmail.com>
2017-04-03 14:30:08 -0700

    add basic diceBox component

--- src/components/CurrentUser.js
+++ src/components/CurrentUser.js
@@ -1,43 +1,44 @@
 import React, { PropTypes } from 'react';
-import {database} from '../firebase';
+import { database } from '../firebase';
 import User from './User';
 
 class CurrentUser extends React.Component {
   constructor(props) {
-    super(props);  
+    super(props);
   }
 
-  render() {        
-    const { auth, signOut } = this.props;    
+
+  render() {
+    console.log('current user props', this.props);
+    const { auth, signOut } = this.props;
     return (
-    <div className="CurrentUser">
-      <img
-        className="CurrentUser--photo"
-        src={ auth.photoURL }
-        alt={ auth.displayName }
-      />
-      <div className="CurrentUser--identification">
-        <h3 className="CurrentUser--displayName">{ auth.displayName }</h3>
-        <p className="CurrentUser--email">{ auth.email }</p>
-        <button
-          className="CurrentUser--signout"          
-          onClick={()=>{ signOut(auth.uid); }}
-        >
+      <div className="CurrentUser">
+        <img
+          className="CurrentUser--photo"
+          src={auth.photoURL}
+          alt={auth.displayName}
+        />
+        <div className="CurrentUser--identification">
+          <h3 className="CurrentUser--displayName">{ auth.displayName }</h3>
+          <p className="CurrentUser--email">{ auth.email }</p>
+          <button
+            className="CurrentUser--signout"
+            onClick={() => { signOut(auth.uid); console.log('im gere', auth); }}
+          >
         Sign Out
         </button>
 
-      {/*{this.props.playersOnline.map(user => {
-        return <p>user</p>; 
+          {/* {this.props.playersOnline.map(user => {
+        return <p>user</p>;
       })}*/}
 
-      {this.props.playersOnline.map(user => {
-        return <div key={user}>{user}</div>;
-      })}
-        
-                
-          
+
+          {this.props.playersOnline.map(user => <div key={user}>{user}</div>)}
+
+          {JSON.stringify(this.props)}
+
+        </div>
       </div>
-    </div>
     );
   }
 }
@@ -48,12 +49,10 @@ CurrentUser.propTypes = {
     displayName: PropTypes.string,
     email: PropTypes.string.isRequired,
     photoURL: PropTypes.string,
-    uid: PropTypes.string.isRequired
+    uid: PropTypes.string.isRequired,
   }),
-  signOut: PropTypes.func.isRequired
+  signOut: PropTypes.func.isRequired,
 };
-
-
 
 
 export default CurrentUser;
---
+++ src/components/DiceBox.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class DiceBox extends Component {
+  render() {
+    return (
+      <div> I am the diceBox div </div>
+    );
+  }
+}
+
+export default DiceBox;
