442bc7ccb78cac7aeae821f12b999fe1ef3e530f
Derek Alia <derekalia@me.com>
2017-04-06 20:25:14 -0700

    React Router

--- src/components/CurrentUser.js
+++ src/components/CurrentUser.js
@@ -11,18 +11,15 @@ class CurrentUser extends React.Component {
     const { auth, signOut } = this.props;
     return (
       <div className="CurrentUser">
-        <img
+        {/* <img
           className="CurrentUser--photo"
           src={auth.photoURL}
           alt={auth.displayName}
-        />
+        />*/}
         <div className="CurrentUser--identification">
           <h3 className="CurrentUser--displayName">{ auth.displayName }</h3>
           <p className="CurrentUser--email">{ auth.email }</p>
-          <button
-            className="CurrentUser--signout"
-            onClick={() => { signOut(auth.uid); console.log('im gere', auth); }}
-          > Sign Out</button>
+          <button className="CurrentUser--signout"onClick={() => { signOut(auth.uid); }}> Sign Out</button>
 
           {this.props.playersOnline.map(user => <div key={user.uid}>{user.displayName}
             <ul>
