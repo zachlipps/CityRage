a4e3aa0ab785f8f84bc6348f81f837d93a889960
Jaime Pericas <jaime.pericas.saez@gmail.com>
2017-04-07 16:23:56 -0700

    disaster code in process

--- src/actions/market.js
+++ src/actions/market.js
@@ -24,23 +24,25 @@ function dealCard(obj) {
 
 // add logic that checks whether the player can buy; and substract the energy
 export const buyCard = (card, buyer) => (dispatch) => {
-  marketRef.once('value', (snapshot) => {
-    const copy = snapshot.val();
-    firebaseFix(copy);
-    copy.face_up = copy.face_up.filter(c => c.title !== card.title);
-    if (copy.deck.length > 0) {
-      dealCard(copy);
-      regenDeckIfEmpty(copy);
+  database.ref('/').once('value', (allData) => {
+    firebaseFix(allData.val().market);
+    const market = allData.val().market;
+    console.log('FIXED MARKET', market);
+    const player = allData.val().users[buyer];
+  // check buyer.uid = current palyer.uid <----------- missing
+    if (player.stats.energy >= card.cost) {
+      player.stats.energy -= card.cost;
+      player.hand.push(card);
+      market.face_up = market.face_up.filter(c => c.title !== card.title);
+      if (market.deck.length > 0) {
+        dealCard(market);
+        regenDeckIfEmpty(market);
+      }
+    } else {
+      console.log('not enough energy');
     }
-    marketRef.set(copy)
-    // when done: set the line bellow inside the check for energy creds after it
-    .then(() => database.ref(`/users/${buyer}/hand`).push(card))
-    .then(() => usersRef.once('value', (users) => {
-      // users.val()[buyer].stats.energy;
-      const energyLeft = users.val()[buyer].stats.energy - card.cost;
-      database.ref(`/users/${buyer}/stats/energy`).set(energyLeft);
-    }))
-    .then(() => dispatch({ type: 'DEAL_CARD', payload: copy }));
+    database.ref('/').set(allData.val())
+    .then(() => dispatch({ type: 'DEAL_CARD', payload: market }));
   });
 };
 
@@ -59,3 +61,25 @@ export const resetMarket = () => (dispatch) => {
     .then(() => dispatch({ type: 'DEAL_NEW_MARKET', payload: copy }));
   });
 };
+
+  // marketRef.once('value', (snapshot) => {
+  //   const copy = snapshot.val();
+  //   firebaseFix(copy);
+  //   copy.face_up = copy.face_up.filter(c => c.title !== card.title);
+  //   if (copy.deck.length > 0) {
+  //     dealCard(copy);
+  //     regenDeckIfEmpty(copy);
+  //   }
+  //   marketRef.set(copy)
+  //   // when done: set the line bellow inside the check for energy creds after it
+  //   .then(() => usersRef.once('value', (users) => {
+  //     if (users.val()[buyer].stats.energy >= card.cost) {
+  //       const energyLeft = users.val()[buyer].stats.energy - card.cost;
+  //       database.ref(`/users/${buyer}/stats/energy`).set(energyLeft)
+  //       .then(() => database.ref(`/users/${buyer}/hand`).push(card);
+  //     } else {
+  //       console.log('not enough energy!!');
+  //     }
+  //   }))
+  //   .then(() => dispatch({ type: 'DEAL_CARD', payload: copy }));
+  // });
