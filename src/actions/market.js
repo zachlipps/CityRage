e02221d8a97c50463db4e742c3915346dbfc1e6c
Jaime Pericas <jaime.pericas.saez@gmail.com>
2017-04-06 20:07:36 -0700

    resetMarket action fully implemented

--- src/actions/market.js
+++ src/actions/market.js
@@ -2,52 +2,63 @@ import shuffle from 'lodash/shuffle';
 import { database } from '../firebase';
 const marketRef = database.ref('/market');
 
-// add logic to get the card to the player's hand
+const currentPlayer = database.ref('/currentPlayer').once('value', snapshot => snapshot.val().uid);
+
+function regenDeckIfEmpty(obj) {
+  if (!obj.deck || obj.deck.length === 0) {
+    obj.deck = [];
+    obj.deck = shuffle(obj.discarded);
+    obj.discarded = [];
+  }
+}
+
+function firebaseFix(obj) {
+  if (!obj.deck) obj.deck = [];
+  if (!obj.face_up) obj.face_up = [];
+  if (!obj.discarded) obj.discarded = [];
+}
+
+function dealCard(obj) {
+  obj.face_up.push(obj.deck[0]);
+  obj.deck.shift();
+}
+
+// this function's content will later be passed into the buy card function...
+export const addToHand = (card, buyer) => {
+  console.log('addToHand fired');
+  return { type: 'w.e.' };
+};
+
+// TODO: add logic to get the card to the player's hand
 export const buyCard = (card, buyer) => (dispatch) => {
   marketRef.once('value', (snapshot) => {
     const copy = snapshot.val();
-    for (let i = 0; i < copy.face_up.length; i++) {
-      if (copy.face_up[i].title === card) {
-        copy.face_up.splice(i, 1);
-      }
+    firebaseFix(copy);
+    copy.face_up = copy.face_up.filter(c => c.title !== card.title);
+    if (copy.deck.length > 0) {
+      dealCard(copy);
+      regenDeckIfEmpty(copy);
     }
-    marketRef.set(copy);
-    dispatch({ type: 'BUY_CARD', payload: copy });
-  });
-};
-
-export const dealCard = () => (dispatch) => {
-  marketRef.once('value', (snapshot) => {
-    const copy = snapshot.val();
-    if (!copy.face_up) copy.face_up = [];
-    if (copy.face_up.length < 3 && copy.deck.length > 0) {
-      const toBeDealt = copy.deck[0];
-      copy.face_up.push(toBeDealt);
-      copy.deck.shift();
-    }
-    marketRef.set(copy);
-    dispatch({ type: 'DEAL_CARD', payload: copy });
+    marketRef.set(copy)
+    .then(() => dispatch({ type: 'DEAL_CARD', payload: copy }));
   });
 };
 
 export const resetMarket = () => (dispatch) => {
   marketRef.once('value', (snapshot) => {
     const copy = snapshot.val();
+    firebaseFix(copy);
     if (!copy.deck) {
       copy.deck = shuffle(copy.discarded);
       copy.discarded = [];
     }
-    if (!copy.face_up) copy.face_up = [];
-    if (!copy.discarded) copy.discarded = [];
     copy.face_up.forEach(card => copy.discarded.push(card));
     copy.face_up = [];
-    const length = copy.deck.length >= 3 ? 3 : copy.deck.length;
-    for (let i = 0; i < length; i++) {
-      copy.face_up.push(copy.deck[0]);
-      copy.deck.splice(0, 1);
+    for (let i = 0; i < 3; i++) {
+      dealCard(copy);
+      regenDeckIfEmpty(copy);
     }
-    marketRef.set(copy);
-    dispatch({ type: 'DEAL_NEW_MARKET', payload: copy });
+    marketRef.set(copy)
+    .then(() => dispatch({ type: 'DEAL_NEW_MARKET', payload: copy }));
   });
 };
-
