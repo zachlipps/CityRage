483bbb3c3adbacf11217cc3f11ae1896be5e4736
Jaime Pericas <jaime.pericas.saez@gmail.com>
2017-04-07 21:10:25 -0700

    market works in game branch; cards are added to hand on buy

--- src/actions/market.js
+++ src/actions/market.js
@@ -6,11 +6,9 @@ const gameId = 'aqwewq334';
 const gameRef = database.ref(`games/${gameId}`);
 
 function firebaseFix(obj) {
-  console.log('fbFix before', obj);
   obj.deck = obj.deck || [];
   obj.face_up = obj.face_up || [];
   obj.discarded = obj.discarded || [];
-  console.log('fbFix after', obj);
 }
 
 function regenDeckIfEmpty(obj) {
@@ -27,25 +25,30 @@ function dealCard(obj) {
 }
 
 // add logic that checks whether the player can buy; and substract the energy
-export const buyCard = (room, card, buyer) => (dispatch) => {
-  // // check buyer.uid = current palyer.uid <----------- missing
-  //   if (player.stats.energy >= card.cost) {
-  //     player.stats.energy -= card.cost;
-  //     player.hand.push(card);
-  //
-  //     if (market.deck.length > 0) {
-  //       dealCard(market);
-  //       regenDeckIfEmpty(market);
-  //     }
-  //   } else {
-  //     console.log('not enough energy');
-  //   }
-  //   database.ref('/').set(allData.val())
-  //   .then(() => dispatch({ type: 'DEAL_CARD', payload: market }));
-  // });
+export const buyCard = (card, buyer, roomId) => (dispatch) => {
+  let market = {};
+  gameRef.once('value', (gameData) => {
+    const consumer = gameData.val().players[buyer];
+    market = gameData.val().market;
+    firebaseFix(market);
+    // must later turn to if energy >= card.cost
+    if (consumer.stats.energy === 0) {
+      // consumer.stats.energy -= card.cost;
+      market.face_up = market.face_up.filter(c => c.title !== card.title);
+      if (!Array.isArray(consumer.hand)) {
+        consumer.hand = [];
+      }
+      consumer.hand.push(card);
+      dealCard(market);
+      regenDeckIfEmpty(market);
+    }
+    gameRef.child('market').set(market)
+    .then(() => gameRef.child('players').child(buyer).set(consumer));
+  })
+  .then(() => dispatch({ type: 'DEAL_CARD', payload: market }));
 };
 
-// "room" parameter to be set to gameID dynamically
+// "room" parameter must be dynamically set to gameID - not yet implemented
 export const resetMarket = () => (dispatch) => {
   gameRef.child('market').once('value', (marketData) => {
     const market = marketData.val();
@@ -60,25 +63,3 @@ export const resetMarket = () => (dispatch) => {
     .then(() => dispatch({ type: 'DEAL_NEW_MARKET', payload: market }));
   });
 };
-
-  // marketRef.once('value', (snapshot) => {
-  //   const copy = snapshot.val();
-  //   firebaseFix(copy);
-  //   copy.face_up = copy.face_up.filter(c => c.title !== card.title);
-  //   if (copy.deck.length > 0) {
-  //     dealCard(copy);
-  //     regenDeckIfEmpty(copy);
-  //   }
-  //   marketRef.set(copy)
-  //   // when done: set the line bellow inside the check for energy creds after it
-  //   .then(() => usersRef.once('value', (users) => {
-  //     if (users.val()[buyer].stats.energy >= card.cost) {
-  //       const energyLeft = users.val()[buyer].stats.energy - card.cost;
-  //       database.ref(`/users/${buyer}/stats/energy`).set(energyLeft)
-  //       .then(() => database.ref(`/users/${buyer}/hand`).push(card);
-  //     } else {
-  //       console.log('not enough energy!!');
-  //     }
-  //   }))
-  //   .then(() => dispatch({ type: 'DEAL_CARD', payload: copy }));
-  // });
