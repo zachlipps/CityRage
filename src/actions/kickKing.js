f9606adf7203d3348aeb5f7c144836d898943ffc
Derek Alia <derekalia@me.com>
2017-04-10 08:48:27 -0700

    Set king now working

--- src/actions/kickKing.js
+++ src/actions/kickKing.js
@@ -1,2 +1,10 @@
+import { database } from '../firebase';
 
+const game = database.ref('games/aqwewq334');
 
+export const setKing = () => {
+  game.child('/chosenOne').once('value', snapshot => snapshot)
+  .then((chosenOne) => {
+    game.child('/king').set(chosenOne.val());
+  });
+};
