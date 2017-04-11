ceb32610ab40b48f5eea9ec74dca9bf54ee13cbc
Derek Alia <derekalia@me.com>
2017-04-11 14:25:01 -0700

    Change stats buttons are now independent of game branch

--- src/actions/energy.js
+++ src/actions/energy.js
@@ -1,13 +1,9 @@
 import { database } from '../firebase';
 
-const game1 = database.ref('games/aqwewq334');
-
 
 export const increaseEnergy = uid => (dispatch, storeState) => {
-  const gid = storeState.val().auth.gid;
-  const game = database.ref(`game/${gid}`);
-
-  console.log('hi', game);
+  const gid = storeState().auth.gid;
+  const game = database.ref(`games/${gid}`);
 
   let energy;
   game.child(`players/${uid}`).once('value', (snapshot) => {
@@ -17,15 +13,16 @@ export const increaseEnergy = uid => (dispatch, storeState) => {
   game.child(`players/${uid}/stats/energy`).set(energy);
 };
 
-export const decreaseEnergy = uid =>
-  // do some stuff in firebase
-   (dispatch) => {
-     let energy;
-     game1.child(`players/${uid}`).once('value', (snapshot) => {
-       energy = snapshot.val().stats.energy;
-       console.log(energy);
-     });
-     energy -= 1;
-     game1.child(`players/${uid}/stats/energy`).set(energy);
-   };
+export const decreaseEnergy = uid => (dispatch, storeState) => {
+  const gid = storeState().auth.gid;
+  const game = database.ref(`games/${gid}`);
+
+  let energy;
+  game.child(`players/${uid}`).once('value', (snapshot) => {
+    energy = snapshot.val().stats.energy;
+    console.log(energy);
+  });
+  energy -= 1;
+  game.child(`players/${uid}/stats/energy`).set(energy);
+};
 
--- src/actions/health.js
+++ src/actions/health.js
@@ -1,28 +1,27 @@
 import { database } from '../firebase';
 
-const game = database.ref('games/aqwewq334');
 
-export const increaseHealth = uid =>
-  // do some stuff in firebase
-   (dispatch) => {
-     let health;
-     game.child(`players/${uid}`).once('value', (snapshot) => {
-       health = snapshot.val().stats.health;
-       console.log(health);
-     });
-     health += 1;
-     game.child(`players/${uid}/stats/health`).set(health);
-   };
+export const increaseHealth = uid => (dispatch, storeState) => {
+  const gid = storeState().auth.gid;
+  const game = database.ref(`games/${gid}`);
+  let health;
+  game.child(`players/${uid}`).once('value', (snapshot) => {
+    health = snapshot.val().stats.health;
+    console.log(health);
+  });
+  health += 1;
+  game.child(`players/${uid}/stats/health`).set(health);
+};
 
 
-export const decreaseHealth = uid =>
-  // do some stuff in firebase
-   (dispatch) => {
-     let health;
-     game.child(`players/${uid}`).once('value', (snapshot) => {
-       health = snapshot.val().stats.health;
-       console.log(health);
-     });
-     health -= 1;
-     game.child(`players/${uid}/stats/health`).set(health);
-   };
+export const decreaseHealth = uid => (dispatch, storeState) => {
+  const gid = storeState().auth.gid;
+  const game = database.ref(`games/${gid}`);
+  let health;
+  game.child(`players/${uid}`).once('value', (snapshot) => {
+    health = snapshot.val().stats.health;
+    console.log(health);
+  });
+  health -= 1;
+  game.child(`players/${uid}/stats/health`).set(health);
+};
