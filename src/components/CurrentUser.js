e2f55715f7f86195b1b5b9da402cde130e0eb46f
Derek Alia <derekalia@me.com>
2017-04-11 14:36:10 -0700

    Fixed buttons on currentUser

--- src/components/CurrentUser.js
+++ src/components/CurrentUser.js
@@ -30,7 +30,7 @@ class CurrentUser extends React.Component {
           </div>)}
 
 
-          {<button onClick={() => { this.props.increaseHealth(auth.uid); }} >up health</button>
+          <button onClick={() => { this.props.increaseHealth(auth.uid); }} >up health</button>
           <button onClick={() => { this.props.increaseHealth(auth.uid); }} >up health</button>
           <button onClick={() => { this.props.decreaseHealth(auth.uid); }}>down health</button>
           <button onClick={() => { this.props.increasePoints(auth.uid); }} >up Points</button>
@@ -39,7 +39,7 @@ class CurrentUser extends React.Component {
           <button onClick={() => { this.props.decreaseEnergy(auth.uid); }}>down Energy</button>
           <button onClick={() => { this.props.joinGame(auth.uid); }}> JOIN GAME </button>
           <button onClick={() => { this.props.leaveGame(auth.uid); }}> LEAVE GAME </button>
-          <button onClick={() => { this.props.changeStat(auth.uid); }}> CHANGE STAT </button>}
+          <button onClick={() => { this.props.changeStat(auth.uid); }}> CHANGE STAT </button>
         </div>
       </div>
     );
