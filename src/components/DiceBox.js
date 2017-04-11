2fbd8e917ea4893313994a921eab0460d5a9f4cc
Westin Funk <westinfunk@gmail.com>
2017-04-11 14:18:49 -0700

    fixed - endTurn button disappears when its not your turn

--- src/components/DiceBox.js
+++ src/components/DiceBox.js
@@ -6,8 +6,13 @@ class DiceBox extends Component {
 
 
   render() {
+
+  let endTurnButton = (this.props.game.chosenOne && this.props.game.chosenOne.uid===this.props.auth.uid) ?
+    <button onClick={() => { this.props.endTurn(); }}> End Turn</button> : undefined;
+    
+  let 
+
     const rolled = this.props.diceBox.one.val;
-    // console.log('dicebox component', this.props);
     return (
       <div>
         <div style={{ display: 'flex', border: '1px solid black' }}>
@@ -26,8 +31,7 @@ class DiceBox extends Component {
           </div>
 
         </div>
-        {(this.props.auth.uid===this.props.game.chosenOne.uid) ? <button onClick={() => { this.props.endTurn(); }}> End Turn</button> : undefined}
-        <button onClick={() => { this.props.endTurn(); }}> End Turn</button>
+        {endTurnButton}
       </div>
     );
   }
