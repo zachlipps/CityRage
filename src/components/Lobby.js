8b0d2bc48b9b7437c056435f5d1f69c1a114bbc0
Jaime Pericas <jaime.pericas.saez@gmail.com>
2017-04-12 11:30:06 -0700

    auto shows players in room

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -3,15 +3,19 @@ import StartGame from './StartGame';
 
 export default class Lobby extends React.Component {
 
+  componentWillMount() {
+    this.props.playersInLobby(this.props.gid);
+  }
+
   render() {
     // console.log(this.props);
     return (
       <div>
         Lobby!
-        <button onClick={() => this.props.playersInLobby(this.props.gid)} >show me whos here!</button>
+        {/* <button onClick={() => this.props.playersInLobby(this.props.gid)} >show me whos here!</button>*/}
 
         {/* show a list of players in the game */}
-        {this.props.lobby.map(person => <div key={person}>{person}</div>)}
+        {this.props.lobby.map(person => <div key={person}>Player: {person}</div>)}
         {/* StartGame button in here*/}
         {/* <button onClick={() => this.props.startGame()}>Start Game</button>*/}
         <button onClick={() => { this.props.leaveGame(this.props.auth.uid); }}>Leave</button>
