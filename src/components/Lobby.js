bfb9f47ab2dcfbf4ccd49a604f19af81ef892fb9
Jaime Pericas <jaime.pericas.saez@gmail.com>
2017-04-12 12:11:57 -0700

    moved button to bottom

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -14,11 +14,10 @@ export default class Lobby extends React.Component {
     return (
       <div>
         <h3>Lobby</h3>
-        <button onClick={() => this.props.playersInLobby(this.props.gid)} >show me whos here!</button>
-
         {/* show a list of players in the game */}
         <h4>Players in lobby:</h4>
         {this.props.lobby.map(person => <div key={person}> Name/ID: {person}</div>)}
+        <button onClick={() => this.props.playersInLobby(this.props.gid)} >show me whos here!</button>
         {/* StartGame button in here*/}
         {/* <button onClick={() => this.props.startGame()}>Start Game</button>*/}
         <Link to="/games-list">
