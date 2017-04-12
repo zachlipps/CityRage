c070f0b6ecc8bd7a49c5ba9049d34826661090f9
Westin Funk <westinfunk@gmail.com>
2017-04-12 16:34:13 -0700

    icons to help know game state

--- src/containers/CurrentUserContainer.js
+++ src/containers/CurrentUserContainer.js
@@ -10,6 +10,7 @@ import { changeStat } from '../actions/changeStat';
 const mapStateToProps = state => ({
   playersOnline: state.playersOnline,
   PlayersInGame: state.PlayersInGame,
+  game: state.game
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
