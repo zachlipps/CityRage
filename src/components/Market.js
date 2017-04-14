daf52d7a8b9b94ae8fa771a8427396a4218fea78
Jaime Pericas <jaime.pericas.saez@gmail.com>
2017-04-14 16:49:33 -0700

    basic styling for market, no card flip yet

--- src/components/Market.js
+++ src/components/Market.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import '../assets/market.css';
+import energy from '../assets/media/energy.png';
 
 class Market extends Component {
   constructor(props) {
     super(props);
-    this.cardStyle = { margin: '10px', width: '100px', height: '120px', border: '1px solid black', float: 'left' };
   }
 
   componentWillMount() {
@@ -13,22 +14,55 @@ class Market extends Component {
 
   createMarket() {
     return this.props.market.face_up.map(card => (
-      <div key={card.title} onClick={() => this.props.buyCard(card, this.props.user, this.props.chosenOne_uid)} className="market-card" style={this.cardStyle}>
-        <div>{card.title}</div>
-        <div>Type: {card.type}</div>
-        <div>Cost: {card.cost}</div>
-        <div>Effect: {card.ability}</div>
+      <div
+        key={card.title}
+        className="market-cards cards"
+        onClick={() => this.props.buyCard(card, this.props.user, this.props.chosenOne_uid)}
+      >
+        <div className="card-cost">
+          <div><img className="cost-logo" src={energy} alt="E" /></div>
+          <div className="cost-logo"> {card.cost}</div>
+        </div>
+
+        <div className="card-title"><div>{card.title}</div></div>
+        <div className="card-ability"><div>{card.ability}</div></div>
+        <div className="card-type">{card.type}</div>
       </div>
       ));
   }
 
   render() {
     return (
-      <div className="market-container" style={{ marginBottom: '25px', margin: '10px', border: '1px solid black', height: '165px', width: '625px' }}>
-        <button onClick={() => this.props.userResetMarket(this.props.user, this.props.chosenOne_uid)}> ResetCards </button><br />
-        <div style={this.cardStyle}>Deck</div>
-        { this.createMarket() }
-        <div style={this.cardStyle}>Discar Pile</div>
+      <div>
+        <div className="market-container">
+          <div>
+            <button
+              id="reset-bttn"
+              onClick={() => this.props.userResetMarket(this.props.user, this.props.chosenOne_uid)}
+            ><div>ResetCards</div>
+              <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                <div className="cost-logo"> —2 </div>
+                <div><img className="cost-logo" src={energy} alt="E" /></div>
+              </div>
+            </button>
+          </div>
+          <br />
+
+          <div className="market-cards deck-disc">
+            <div>Deck</div>
+            <div>{this.props.market.deck.length} cards</div>
+          </div>
+
+          { this.createMarket() }
+
+          <div className="market-cards deck-disc">
+            <div>Discar Pile</div>
+            <div>{this.props.market.discarded.length} cards</div>
+          </div>
+
+
+        </div>
+
       </div>
     );
   }
